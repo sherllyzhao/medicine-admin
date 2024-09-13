95c46bcb6ff9f42c08a778b69d3c1d58869ccfa4
sherllyzhao <1253108690@qq.com>
2024-09-13 06:48:21 +0000

    1

--- db/config/config.js
+++ db/config/config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  DBHOST: 'localhost',
-  DBPORT: 27017,
-  DBNAME: 'medicine',
-}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sherllyzhao:sherllyzhao@sherllyzhao.6litq.mongodb.net/?retryWrites=true&w=majority&appName=sherllyzhao";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+module.exports = client
--- db/db/db.js
+++ db/db/db.js
@@ -1,31 +1,21 @@
 module.exports = function (success, error) {
-  // 1. 导入mongoose模块
-  const mongoose = require("mongoose");
+  const client = require('../config/config');
 
-  const config = require('../config/config');
-
-  const {DBHOST, DBPORT, DBNAME} = config
-
-  // 2. 连接数据库，如果数据库不存在，则创建
-  mongoose.connect(`mongodb://${DBHOST}:${DBPORT}/${DBNAME}`);
-
-  // 3. 回调 once 只执行一次
-  mongoose.connection.once("open", () => {
-    console.log("连接成功");
-    success && success()
-  });
-
-  mongoose.connection.once("error", () => {
-    console.log("连接失败");
-    error && error()
-  });
-
-  mongoose.connection.once("close", () => {
-    console.log("连接关闭");
-  });
-
-  // 4. 关闭连接
-  /* setTimeout(() => {
-  mongoose.disconnect();
-}) */
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("连接成功");
+      success && success();
+    } catch (e) {
+      console.error(e);
+      error && error();
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 };
